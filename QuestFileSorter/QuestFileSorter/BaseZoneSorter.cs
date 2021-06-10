db96cf59009e16a401de62c9baf8123a7478e4c7
ArmatasAnthony <armatasanthony@gmail.com>
2021-06-09 19:57:34 -0700

    Honestly, I do not remember doing this, nor what this does. However this is sitting in a local repo and I have a feeling I may need this for whatever reason in the future. Who knwos.

---
+++ QuestFileSorter/QuestFileSorter/BaseZoneSorter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace QuestFileSorter
+{
+    public class BaseZoneSorter
+    {
+        public string checkZoneForQuests(string ZoneName, StreamWriter LoggingObj, string pageUrl)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            Console.SetBufferSize(Console.BufferWidth, 32766);
+
+            //string baseQuestWebAddress = "https://classic.wowhead.com/elwynn-forest-quests";
+            WebClient client = new WebClient();
+            string viewPageSourceText = client.DownloadString(pageUrl);
+            //Regex101 
+            //(new Listview[\s\S]*<div class=\\\"clear\\\"><\/div>)
+            string questSectionRegex = "(new Listview[\\s\\S]*<div class=\\\"clear\\\"><\\/div>)";
+            Regex rx = new Regex(questSectionRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            MatchCollection sectionmatches = rx.Matches(viewPageSourceText);
+            string QuestIDSection = sectionmatches[0].Groups[1].Value;
+            string getQuestIDRegex = "\\\"id\\\":([\\d]*),";
+            rx = new Regex(getQuestIDRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            sectionmatches = rx.Matches(QuestIDSection);
+            Console.WriteLine();
+
+            string audioFileLocation = "G:\\Workshop\\QuestHerald\\CompressedAllInOne";
+            DirectoryInfo dirInfo = new DirectoryInfo(audioFileLocation);//Assuming Test is your Folder
+
+            for (int curID = 0; curID < sectionmatches.Count; curID++)
+            {
+                string curQuestID = sectionmatches[curID].Groups[1].Value;
+                FileInfo[] Files = dirInfo.GetFiles(curQuestID + "_*.mp3"); //Getting mp3 files
+                
+                string str = "";
+                foreach (FileInfo file in Files)
+                {
+                    try
+                    {
+                        file.CopyTo(@"F:\Workshop\QuestHerald 1.1.1\QuestAudio\" + file.Name);
+                        LoggingObj.WriteLine(curQuestID);
+                    }
+                    catch (Exception ex)
+                    {
+                        LoggingObj.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            //string audioFileLocation = "G:\\Workshop\\QuestHerald\\CompressedAllInOne";
+            //DirectoryInfo dirInfo = new DirectoryInfo(audioFileLocation);//Assuming Test is your Folder
+            //FileInfo[] Files = dirInfo.GetFiles("*.mp3"); //Getting mp3 files
+            //FileInfo[] Files2 = dirInfo.GetFiles("2_*.mp3"); //Getting mp3 files
+
+            //string str = "";
+            //var fasf = Files.ToHashSet();
+            //foreach (FileInfo file in Files)
+            //{
+            //    str = str + ", " + file.Name;
+            //    //Regex rx = new Regex(questSectionRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            //    //MatchCollection sectionmatches = rx.Matches(viewPageSourceText);
+
+            //}
+
+            //Need to walk through each of the quest IDs retrived
+            //And move their audio files into a new file
+
+            //Need to get a list of each zone and do that for each zone.
+
+            return "ZoneQuests";
+        }
+    }
+}
---
+++ QuestFileSorter/QuestFileSorter/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace QuestFileSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] zoneQuestUrls = new string[]
+            {
+                "https://classic.wowhead.com/alterac-mountains-quests",
+                "https://classic.wowhead.com/alterac-valley-eastern-kingdoms-quests",
+                "https://classic.wowhead.com/arathi-highlands-quests",
+                "https://classic.wowhead.com/badlands-quests",
+                "https://classic.wowhead.com/blackrock-mountain-quests",
+                "https://classic.wowhead.com/blasted-lands-quests",
+                "https://classic.wowhead.com/burning-steppes-quests",
+                "https://classic.wowhead.com/deeprun-tram-quests",
+                "https://classic.wowhead.com/dun-morogh-quests",
+                "https://classic.wowhead.com/duskwood-quests",
+                "https://classic.wowhead.com/eastern-plaguelands-quests",
+                "https://classic.wowhead.com/elwynn-forest-quests",
+                "https://classic.wowhead.com/hillsbrad-foothills-quests",
+                "https://classic.wowhead.com/ironforge-quests",
+                "https://classic.wowhead.com/kharanos-quests",
+                "https://classic.wowhead.com/loch-modan-quests",
+                "https://classic.wowhead.com/redridge-mountains-quests",
+                "https://classic.wowhead.com/searing-gorge-quests",
+                "https://classic.wowhead.com/eastern-kingdoms-shadowfang-keep-quests",
+                "https://classic.wowhead.com/silverpine-forest-quests",
+                "https://classic.wowhead.com/stormwind-city-quests",
+                "https://classic.wowhead.com/stranglethorn-vale-quests",
+                "https://classic.wowhead.com/swamp-of-sorrows-quests",
+                "https://classic.wowhead.com/hinterlands-quests",
+                "https://classic.wowhead.com/tirisfal-glades-quests",
+                "https://classic.wowhead.com/undercity-quests",
+                "https://classic.wowhead.com/western-plaguelands-quests",
+                "https://classic.wowhead.com/westfall-quests",
+                "https://classic.wowhead.com/wetlands-quests",
+                "https://classic.wowhead.com/ashenvale-quests",
+                "https://classic.wowhead.com/azshara-quests",
+                "https://classic.wowhead.com/darkshore-quests",
+                "https://classic.wowhead.com/darnassus-quests",
+                "https://classic.wowhead.com/desolace-quests",
+                "https://classic.wowhead.com/durotar-quests",
+                "https://classic.wowhead.com/dustwallow-marsh-quests",
+                "https://classic.wowhead.com/felwood-quests",
+                "https://classic.wowhead.com/feralas-quests",
+                "https://classic.wowhead.com/moonglade-quests",
+                "https://classic.wowhead.com/mulgore-quests",
+                "https://classic.wowhead.com/orgrimmar-quests",
+                "https://classic.wowhead.com/ruttheran-village-quests",
+                "https://classic.wowhead.com/silithus-quests",
+                "https://classic.wowhead.com/stonetalon-mountains-quests",
+                "https://classic.wowhead.com/tanaris-quests",
+                "https://classic.wowhead.com/teldrassil-quests",
+                "https://classic.wowhead.com/barrens-quests",
+                "https://classic.wowhead.com/thousand-needles-quests",
+                "https://classic.wowhead.com/thunder-bluff-quests",
+                "https://classic.wowhead.com/timbermaw-hold-quests",
+                "https://classic.wowhead.com/ungoro-crater-quests",
+                "https://classic.wowhead.com/winterspring-quests",
+            };
+            BaseZoneSorter Sorter = new BaseZoneSorter();
+            string logPath = @"F:\Workshop\QuestHerald 1.1.1\QuestAudio\RunTimeLog.txt";
+            using (StreamWriter tw = new StreamWriter(logPath, true))
+            {
+                tw.WriteLine("Quests Added");
+                foreach(string pageUrl in zoneQuestUrls)
+                {
+                    Sorter.checkZoneForQuests(pageUrl, tw, pageUrl);
+                }
+
+            }
+        }
+    }
+}
